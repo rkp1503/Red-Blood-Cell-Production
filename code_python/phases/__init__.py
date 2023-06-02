3f6c6ccd3aeabf482a90b0a14312076cc15d6ae5
Rayla Kurosaki 『黒崎 レイラ』 <60267154+RaylaKurosaki1503@users.noreply.github.com>
2023-06-02 03:24:55 -0400

    Create __init__.py

---
+++ code_python/phases/__init__.py
@@ -0,0 +1,13 @@
+"""
+Author: Ramsey (Rayla) Phuc
+
+Alias: Rayla Kurosaki
+
+GitHub: https://github.com/rkp1503
+"""
+
+# Built-in libraries/modules.
+import os
+import sys
+
+sys.path.append(os.getcwd())
