df413b950867dcb12515b35519fd7016288bb653
Rayla Kurosaki 『黒崎 レイラ』 <60267154+rkp1503@users.noreply.github.com>
2023-06-07 02:23:50 -0400

    Update utils.jl

--- code_julia/utils.jl
+++ code_julia/utils.jl
@@ -9,11 +9,10 @@ module utils
 import Plots: plot, plot!
 
     function myPlot(tₘₐₓ, t_arr, R_arr, γ_colors, filename, normalize=1)
-        figure = Plots.plot(
+        figure = plot(
             title="Red Blood Cell Population over $tₘₐₓ Days",
             xaxis="Time in days",
             yaxis="Red Blood Cell Population",
-
             # legend=:outertopright, 
             background_color_inside="#DDDDDD", 
             gridalpha=1,
@@ -22,13 +21,12 @@ import Plots: plot, plot!
             minorgrid=true,
             minorgridalpha=0.5,
             foreground_color_minor_grid=:"#FF0000",
-            
             # gridalpha=0,
             )
         for (tₛ, Rₛ, γ_color) in zip(t_arr, R_arr, γ_colors)
             label, color = γ_color
-            Plots.plot!(
-                figure, tₛ, Rₛ/normalize, label="γ=$label", linecolor=color, 
+            plot!(
+                figure, tₛ, Rₛ/normalize, label="γ=$label",linecolor=color, 
                 linewidth=2
                 )
         end
