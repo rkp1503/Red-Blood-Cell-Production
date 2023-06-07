570cbc710c530e481596e6535ebe87174a84fe81
Rayla Kurosaki 『黒崎 レイラ』 <60267154+rkp1503@users.noreply.github.com>
2023-06-07 01:31:12 -0400

    Update utils.jl

--- code_julia/utils.jl
+++ code_julia/utils.jl
@@ -6,29 +6,33 @@ Project: Red Blood Cell Production
 =============================================================================#
 
 module utils
-    using Plots
+import Plots
 
-    export myPlot
     function myPlot(tₘₐₓ, t_arr, R_arr, γ_colors, filename, normalize=1)
         figure = Plots.plot(
-            title="Red Blood Cell Population over $tₘₐₓ Days", 
-            xaxis="Time in days", 
-            yaxis="Red Blood Cell Population", 
-            legend=:outertopright,  
-            foreground_color_grid="#000000", 
-            gridalpha=1, 
-            gridlinewidth=:1.5, 
-            minorgrid=true, 
-            foreground_color_minor_grid=:"#FF0000", 
-            minorgridalpha=0.75
+            title="Red Blood Cell Population over $tₘₐₓ Days",
+            xaxis="Time in days",
+            yaxis="Red Blood Cell Population",
+
+            # legend=:outertopright, 
+            background_color_inside="#DDDDDD", 
+            gridalpha=1,
+            gridlinewidth=:1,
+            foreground_color_grid="#000000",
+            minorgrid=true,
+            minorgridalpha=0.5,
+            foreground_color_minor_grid=:"#FF0000",
+            
+            # gridalpha=0,
             )
         for (tₛ, Rₛ, γ_color) in zip(t_arr, R_arr, γ_colors)
             label, color = γ_color
             Plots.plot!(
-                figure, tₛ, Rₛ/normalize, label=label, linecolor=color, 
+                figure, tₛ, Rₛ/normalize, label="γ=$label", linecolor=color, 
                 linewidth=2
                 )
         end
         return figure
     end;
+
 end
