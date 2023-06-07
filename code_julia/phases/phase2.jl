07fa536020b88334f572cde1a0f104b00d7b4f68
Rayla Kurosaki 『黒崎 レイラ』 <60267154+rkp1503@users.noreply.github.com>
2023-06-07 02:23:49 -0400

    Update phase2.jl

--- code_julia/phases/phase2.jl
+++ code_julia/phases/phase2.jl
@@ -12,10 +12,8 @@ using DifferentialEquations: ODEProblem, solve
         initial_vector = collect(values(vars_dict))
         parameter_values = collect(values(params_dict))
         t_span = (0, days)
-        problem = DifferentialEquations.ODEProblem(
-            model, initial_vector, t_span, parameter_values
-            )
-        return DifferentialEquations.solve(problem, reltol=1e-8, abstol=1e-8)
+        problem = ODEProblem(model, initial_vector, t_span, parameter_values)
+        return solve(problem, reltol=1e-8, abstol=1e-8)
     end;
     
 end
